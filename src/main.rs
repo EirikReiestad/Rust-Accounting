b94f8459498e3be5ee1c4cfd45d2a2ea94375a70
eirikreiestad <eirik@reiestad.no>
2022-06-27 17:39:54 +0200

    tried something

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-use eframe::egui;
-
 mod accounting;
 mod excel;
 mod file;
 mod settings;
+
+use eframe::egui;
 
 fn main() {
     let options = eframe::NativeOptions::default();
