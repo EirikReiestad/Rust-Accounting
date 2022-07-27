e86eaf1fb11e2aa2f5f76d74b071c62238f15403
eirikreiestad <eirik@reiestad.no>
2022-07-27 12:25:01 +1000

    mock transactions

--- src/excel/test_setup.rs
+++ src/excel/test_setup.rs
@@ -1,5 +1,6 @@
-use super::workbook;
 use super::reading;
+use super::workbook;
+use chrono::NaiveDate;
 use std::fs;
 use std::sync::Once;
 
@@ -26,7 +27,7 @@ pub fn initialize_tests() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-struct mock_transaction {
+pub struct MockTransaction {
     path: String,
     info: workbook::WorkbookInfo,
     categories: reading::Categories,
@@ -36,7 +37,49 @@ struct mock_transaction {
     date_capitalize: bool,
 }
 
-pub fn create_mock_transactions() -> mock_transaction {
-    let path = "test.xlsx";
-    let transaction_path = ""
+pub fn create_mock_transactions() -> Result<MockTransaction, Box<dyn std::error::Error>> {
+    let path = String::from("test.xlsx");
+    let size = 1000;
+    let mut accounting_date = vec![];
+    let mut interest_date = vec![];
+    let mut archive_reference = vec![];
+    let mut counter_account = vec![];
+    let mut types = vec![];
+    let mut text = vec![];
+    let mut out_of_account = vec![];
+    let mut into_account = vec![];
+    let mut account = vec![];
+    for i in 0..size {
+        accounting_date.push(NaiveDate::from_num_days_from_ce(i));
+        interest_date.push(NaiveDate::from_num_days_from_ce(i));
+        archive_reference.push(String::from("0"));
+        counter_account.push(String::from("1"));
+        types.push(String::from("types"));
+        text.push(String::from("text"));
+        out_of_account.push(0.0);
+        into_account.push(0.0);
+        account.push(String::from("0"))
+    }
+    let info = workbook::WorkbookInfo::new(
+        accounting_date,
+        interest_date,
+        archive_reference,
+        counter_account,
+        types,
+        text,
+        out_of_account,
+        into_account,
+        account,
+    )?;
+
+    let mock_transaction = MockTransaction {
+        path: path,
+        info: info,
+        categories: reading::Categories::new(vec![], vec![]),
+        date_delimiter: String::from("/"),
+        date_month_style: String::from("short"),
+        date_language: String::from("english"),
+        date_capitalize: false,
+    };
+    Ok(mock_transaction)
 }
